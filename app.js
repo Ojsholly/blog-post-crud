f39f711c62106d2cce1016345f3a0825cd8273fb
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2024-09-14 21:40:28 +0100

    Added additional route for practice.

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
+
 //Routes
 app.get('/', (req, res) => {
     res.send("We are on the home page");
+});
+
+app.get('/posts', (req, res) => {
+    res.send("We are on the posts page");
 });
 
 
